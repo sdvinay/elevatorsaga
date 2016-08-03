55565aef66911256729238bdc8b684a3cbb054df
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2016-08-02 23:56:04 -0700

    Collapse duplication between [down|up]_button_pressed callbacks

--- elevator.js
+++ elevator.js
@@ -42,29 +42,22 @@
             var y = function() {
                 var floor = floors[f];
                 var floorNum = f;
-                floor.on("down_button_pressed", function() {
-                    if (floorsWaiting.indexOf(floorNum) === -1) {
-                        floorsWaiting.push(floorNum);
-                    }
-                    console.log(`Down pressed on floor ${floorNum}; floorsWaiting = ${floorsWaiting}`)
-                    if (idleElevators.length > 0) {
-                        elevators[idleElevators.shift()].goToFloor(floorNum);
-                    }
-                });
+                var onButtonPressed = function(direction) {
+                    return function() {
+                        if (floorsWaiting.indexOf(floorNum) === -1) {
+                            floorsWaiting.push(floorNum);
+                        }
+                        console.log(`${direction} pressed on floor ${floorNum}; floorsWaiting = ${floorsWaiting}`)
+                        if (idleElevators.length > 0) {
+                            elevators[idleElevators.shift()].goToFloor(floorNum);
+                        }
 
-                floor.on("up_button_pressed", function() {
-                    if (floorsWaiting.indexOf(floorNum) === -1) {
-                        floorsWaiting.push(floorNum);
                     }
-                    console.log(`Up   pressed on floor ${floorNum}; floorsWaiting = ${floorsWaiting}`)
-                    if (idleElevators.length > 0) {
-                        elevators[idleElevators.shift()].goToFloor(floorNum);
-                    }
-                });
-
+                };
+                floor.on("down_button_pressed", onButtonPressed('down'));
+                floor.on("up_button_pressed"  , onButtonPressed('up'  ));
             }();
         }
-
     },
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
