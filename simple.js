059cde5374bde308fb7ad8e5270cdf6e4997a12d
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2016-09-30 20:29:05 -0700

    Code cleanup: remove dead code, fix formatting and comments

--- simple.js
+++ simple.js
@@ -1,40 +1,35 @@
 {
     init: function(elevators, floors) {
 
-        // Elevator event listeners; attaching the same code/listeners to each elevator here:
-	    _.each(elevators, function (elevator, elevatorNum) {
-                elevator.debug = function() {
-                    var debugStr = `Elevator ${elevatorNum} on floor ${elevator.currentFloor()}: \
-                        DestQueue = ${elevator.destinationQueue}`; 
-                    console.log(debugStr);
-                }
+        // Elevator event listeners and functions
+        _.each(elevators, function(elevator, elevatorNum) {
+            elevator.debug = function() {
+                var debugStr = `Elevator ${elevatorNum} on floor ${elevator.currentFloor()}: \
+                        DestQueue = ${elevator.destinationQueue}`;
+                console.log(debugStr);
+            }
 
-                elevator.call = function(floorNum) {
-                    elevator.goToFloor(floorNum);
-                }
-
-                elevator.on("floor_button_pressed", function(floorNum) {
-                    console.log(`Elevator ${elevatorNum}: floor button pressed for ${floorNum}; `);
-					elevator.goToFloor(floorNum);
-					elevator.debug();
-                });
-
-
+            elevator.on("floor_button_pressed", function(floorNum) {
+                console.log(`Elevator ${elevatorNum}: floor button pressed for ${floorNum}; `);
+                elevator.goToFloor(floorNum);
+                elevator.debug();
             });
+        });
 
         // Floor event listeners
-		_.each(floors, function(floor) {
-                var floorNum = floor.floorNum();
-                var onCallButtonPressed = function(direction) {
-                    return function() {
-                        console.log(`${direction} pressed on floor ${floorNum}; `);
-						elevators[0].goToFloor(floorNum);
-                    }
-                };
-                floor.on("down_button_pressed", onCallButtonPressed('down'));
-                floor.on("up_button_pressed", onCallButtonPressed('up'));
-            });
+        _.each(floors, function(floor) {
+            var floorNum = floor.floorNum();
+            var onCallButtonPressed = function(direction) {
+                return function() {
+                    console.log(`${direction} pressed on floor ${floorNum}; `);
+                    elevators[0].goToFloor(floorNum);
+                }
+            };
+            floor.on("down_button_pressed", onCallButtonPressed('down'));
+            floor.on("up_button_pressed", onCallButtonPressed('up'));
+        });
     },
+
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
     }
